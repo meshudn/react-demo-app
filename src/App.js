51d9962c672ff06999270702852faf5a8593d97b
Meshu Deb Nath <meshu.uiu@gmail.com>
2020-03-02 00:51:42 +0100

    log in/out && state changing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+   constructor(){
+     super();  
+     this.state = {
+          isLoggedIn: false
+      };
+      this.handleClickEvent = this.handleClickEvent.bind(this);
+   }
+   handleClickEvent(){
+     this.setState(prevState => {
+       return {
+         isLoggedIn: !prevState.isLoggedIn
+       };
+     });
+   }
+    render(){
+      let buttonText = this.state.isLoggedIn ? "Log Out" : "Log In";
+      return (
+        <div>
+          <button onClick={this.handleClickEvent}>{buttonText}</button>
+        </div>
+      );
+    }
 }
 
 export default App;
