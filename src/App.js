b7d45b7a708ee11059ca04609be954dc3f0ef5ac
Meshu Deb Nath <meshu.uiu@gmail.com>
2020-03-02 18:51:43 +0100

    form manipulation

--- src/App.js
+++ src/App.js
@@ -4,23 +4,62 @@ class App extends React.Component{
    constructor(){
      super();  
      this.state = {
-          isLoggedIn: false
+          firstName: "",
+          lastName: "",
+          age: "",
+          gender: ""
       };
-      this.handleClickEvent = this.handleClickEvent.bind(this);
+      this.handleChange = this.handleChange.bind(this);
    }
-   handleClickEvent(){
-     this.setState(prevState => {
-       return {
-         isLoggedIn: !prevState.isLoggedIn
-       };
-     });
-   }
+
+   handleChange(event){
+    //  console.log(event.target.value);
+    const {name, value} = event.target;
+      this.setState ({
+          [name]: value,
+    });
+   
+
+  }
     render(){
-      let buttonText = this.state.isLoggedIn ? "Log Out" : "Log In";
+    
       return (
-        <div>
-          <button onClick={this.handleClickEvent}>{buttonText}</button>
-        </div>
+        <form onSubmit={this.handleSubmit}>
+          <label>First Name:</label>
+          <input 
+             type="text" 
+             onChange={this.handleChange} 
+             name="firstName"  
+          /><br/>
+          <label>Last Name:</label>
+          <input 
+             type="text" 
+             onChange={this.handleChange} 
+             name="lastName"  /><br/>
+          <label>Age:</label>
+          <input 
+             type="number" 
+             onChange={this.handleChange} 
+             name="age" /><br/>
+          
+          <label>Gender:</label><br/>
+          <input 
+             type="radio" 
+             checked={this.state.gender === "male"} 
+             onChange={this.handleChange} 
+             value="male"
+             name="gender" /><label>Male</label><br/>
+          <input 
+             type="radio" 
+             checked={this.state.gender === "female"}
+             value="female"
+             onChange={this.handleChange} 
+             name="gender" /><label>Female</label><br/>
+          
+          <div>
+              <h3>{this.state.firstName} {this.state.lastName} is {this.state.age} years old</h3>
+          </div>
+        </form>
       );
     }
 }
